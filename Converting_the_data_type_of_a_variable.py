338ef3367af58daf4fc9f26757421165eea3daee
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 17:56:39 -0500

    It's been created a program that specifies how to convert a numeric variable to a string

---
+++ Converting_the_data_type_of_a_variable.py
@@ -0,0 +1,9 @@
+#This is to convert the data type of a variable
+x=8 #Let's suppose x=8
+#Now we want to convert the data type of x from being an integer variable to a string
+#First, let's verify the type of variable x is
+print(type(x))
+#After we verify x is a numeric variable, we can convert it to string by executing:
+x=str(x)
+#And now, x is a string, let's verify that by executing:
+print(type(x))
